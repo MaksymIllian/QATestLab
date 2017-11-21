2799c8ab38230919cb5a54da1e03d814876f32e7
maximum <nezloy00@mail.ru>
2017-11-21 23:29:35 +0200

    Lab3 v1.0

---
+++ Lab3/src/main/java/utils/EnvironmentVariables.java
@@ -0,0 +1,16 @@
+package utils;
+
+/**
+ * Created by User on 15.11.2017.
+ */
+public enum EnvironmentVariables {
+    BASE_ADMIN_URL("env.admin.url"),
+    BROWSER("browser");
+
+    private String value;
+    EnvironmentVariables(String value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {return  value;};
+}
